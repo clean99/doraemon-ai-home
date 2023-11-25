84f25cc894194b5a1b90acdd585466577963c750
clean99 <xff9924@gmail.com>
2023-11-25 17:39:34 +0800

    feat: init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,39 @@
+import { Button } from '@/components/ui/button'
+import { IoLogoDiscord } from "react-icons/io5";
+import { IoLogoTwitter } from "react-icons/io5";
+import { IoLogoGithub } from "react-icons/io5";
+import { FaExternalLinkAlt } from "react-icons/fa";
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-     
+    <main className="flex min-h-screen flex-col items-center justify-center p-12 ">
+      {/* <Button className='m-2 rounded-full dark:text-white' variant="outline" size="icon" onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>{
+        theme === "dark" ? <CiDark /> : <CiLight />
+      }</Button> */}
+     <h1 className="text-5xl font-bold mb-4" >
+        Next Generation
+        <div className="flex items-center text-blue-600">
+          Doraemon AI <Image className="ml-4" src="/logo.png" alt="logo" width={64} height={64} />
+        </div>
+     </h1>
+      <p className="text-xs text-center text-gray-500">
+        Doraemon AI is a next generation of trustworthy real-time AI-powered personalizationand recommendation platform for eCommerce, FinTech, EdTech and data-driven SaaS.
+      </p>
+      <div className="flex flex-wrap mt-4 justify-center">
+        <Button className='m-2' variant="outline" size="sm">
+          Discord <IoLogoDiscord className="ml-1" />
+        </Button>
+        <Button className='m-2' variant="outline" size="sm">
+          Twitter <IoLogoTwitter className="ml-1" />
+        </Button>
+        <Button className='m-2' variant="outline" size="sm">
+          Github <IoLogoGithub className="ml-1" />
+        </Button>
+        <Button className='m-2' variant="outline" size="sm">
+          Careers <FaExternalLinkAlt className="ml-1" />
+        </Button>
+      </div>
     </main>
   )
 }
